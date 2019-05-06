f361f2c5afca86b8bafa014a903374a762b4dff6
xxUNLINKxx <49578656+xxUNLINKxx@users.noreply.github.com>
2019-05-06 19:48:27 +0800

    Added Chest Script

--- PacWiz/Assets/scripts/interactable/ChestScript.cs
+++ PacWiz/Assets/scripts/interactable/ChestScript.cs
@@ -4,15 +4,45 @@ using UnityEngine;
 
 public class ChestScript : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public chestScriptable GetChest;
+    private bool locked;
+    public int lootAmt;
+    public GameObject[] content;
 
-    // Update is called once per frame
-    void Update()
+    private void Start()
     {
-        
+        locked = GetChest.Locked;
+    }
+    void Chest()
+    {
+        Transform playerPos = GameObject.Find("Player").transform;
+        Vector2 distance = new Vector2(Mathf.Abs(playerPos.position.x + 1), playerPos.position.y);
+        if (!locked)
+        {
+            if (lootAmt > 0)
+            {
+                if (content.Length > 1)
+                {
+                    int rand = Random.Range(0, content.Length + 1);
+                    Instantiate(content[rand], distance, Quaternion.Euler(new Vector3(0, 0, 0)));
+                }
+                else
+                {
+                    Instantiate(content[0], distance, Quaternion.Euler(new Vector3(0, 0, 0)));
+                }
+            }
+
+        }
+    }
+    private void OnTriggerStay2D(Collider2D other)
+    {
+        if (other.CompareTag("Player") || other.CompareTag("Fairy"))
+        {
+            if (Input.GetKeyDown(KeyCode.E))
+            {
+                Chest();
+                lootAmt -= 1;
+            }
+        }
     }
 }
